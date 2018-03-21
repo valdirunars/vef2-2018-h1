5596cf474f60911f42b248ff949d33c6b8b12597
Drífa Sóley Heimisdóttir <drifasoley@Drifas-MacBook-Pro.local>
2018-03-21 20:14:53 +0000

    WIP Resign style

--- Public/scripts/account.js
+++ Public/scripts/account.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
   let usernameInput = $('#account-username-input');
   let nameText = $('#account-name-text');
   let nameInput = $('#account-name-input');
+  let passwordHeader = $('#account-password-h');
+  let passwordInput = $('#account-password-input');
+  let verPassHeader = $('#account-verPass-h');
+  let verPassInput = $('#account-verPass-input')
 
   let editButton = $('#edit-button');
 
@@ -13,11 +17,21 @@ $(document).ready(function () {
       usernameInput.show();
       nameText.hide();
       nameInput.show();
+
+      passwordHeader.show();
+      passwordInput.show();
+      verPassHeader.show();
+      verPassInput.show();
     } else {
       usernameText.show();
       usernameInput.hide();
       nameText.show();
       nameInput.hide();
+
+      passwordHeader.hide();
+      passwordInput.hide();
+      verPassHeader.hide();
+      verPassInput.hide();
     }
   });
 
